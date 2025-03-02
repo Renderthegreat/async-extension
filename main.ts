471c075374a5f4f21f510aa1c24ef89041a95e0e
renderthegreat <renderthegreat.business@gmail.com>
2025-03-02 13:49:34 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,96 @@
+namespace Asynchronous {
+    type Executor<T> = (resolve: (value: T) => void, reject: (reason?: any) => void) => void;
+    type ThenHandler<T, U> = (value: T) => U | Promise<U>;
+    type CatchHandler<U> = (reason?: any) => U | Promise<U>;
+    //% blockId=create_promise block="Create promise with executor %executor"
+    export class Promise<T> {
+        /**
+        * Runs code asynchronously, and returns a result
+        * @returns { Promise }
+        */
+        constructor(executor: Executor<T>) {
+            try {
+                executor((r: any) => this.resolve(r), (r: any) => this.reject(r)); // For Lambda bypass
+            } catch (error) {
+                this.reject(error);
+            };
+        };
+
+        /**
+         * Resolves the promise with the given value
+         * @param value The value for the promise
+         */
+        private resolve(value: T) {
+            if (this.state !== 'pending') return;
+            this.state = 'fulfilled';
+            this.value = value;
+
+            setTimeout(() => {
+                this.thenHandlers.forEach(handler => handler(value));
+            }, 0);
+        };
+
+        /**
+         * Rejects the promise with the given value
+         * @param value The value for the rejection
+         */
+        private reject(reason: any) {
+            if (this.state !== 'pending') return;
+            this.state = 'rejected';
+            this.reason = reason;
+
+            setTimeout(() => {
+                if (this.catchHandler) {
+                    this.catchHandler(reason);
+                } else {
+                    throw `Unhandled Promise rejection: ${reason}`;
+                };
+            }, 0);
+        };
+
+        /**
+         * Sets a callback for when the promise resolves
+         * @param onFulfilled The function to run when the promise is completed
+         * @returns { Promise<U> } The promise of the promise completion
+         */
+        then<U>(onFulfilled: ThenHandler<T, U>): Promise<U> {
+            return new Promise<U>((resolve, reject) => {
+                this.thenHandlers.push(value => {
+                    try {
+                        resolve(onFulfilled(value) as any);
+                    } catch (error2) {
+                        reject(error2);
+                    }
+                });
+                if (this.state === 'fulfilled' && this.value !== undefined) {
+                    resolve(onFulfilled(this.value) as any);
+                }
+            });
+        };
+
+        /**
+         * Sets a callback for when the promise rejects
+         * @param onFulfilled The function to run when the promise fails
+         * @returns { Promise<U> } The promise of the promise failure
+         */
+        catch<U>(onRejected: CatchHandler<U>): Promise<U> {
+            return new Promise<U>((resolve, reject) => {
+                this.catchHandler = reason => {
+                    try {
+                        resolve(onRejected(reason) as any);
+                    } catch (error3) {
+                        reject(error3);
+                    };
+                };
+                if (this.state === 'rejected' && this.reason !== undefined) {
+                    resolve(onRejected(this.reason) as any);
+                }
+            });
+        };
+        private value?: T;
+        private reason?: any;
+        private state: 'pending' | 'fulfilled' | 'rejected' = 'pending';
+        private thenHandlers: ThenHandler<T, any>[] = [];
+        private catchHandler?: CatchHandler<any>;
+    };
+}
