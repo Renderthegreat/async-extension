3ecae155106266e4eaa316184f63fbaa20923b47
renderthegreat <renderthegreat.business@gmail.com>
2025-03-02 14:32:50 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -95,6 +95,12 @@ namespace Asynchronous {
 };
 
 namespace Asynchronous {
+    export enum CompleteOperation {
+        //% block="Resolve"
+        Resolve,
+        //% block="Reject"
+        Reject,
+    };
     //% blockId=create_promise block="Create promise with executor %executor"
     export function createPromise<T>(target: Asynchronous.Executor<T>) {
         return new Asynchronous.Promise(target);
@@ -106,5 +112,9 @@ namespace Asynchronous {
     //% blockId=on_promise_reject block="Set a callback %callback for promise %promise reject"
     export function onPromiseReject<T>(promise: Asynchronous.Promise<T>, rejectCallback: Asynchronous.CatchHandler<any>) {
         promise.catch(rejectCallback);
+    };
+    //% blockId=complete="Complete a promise %parray with operation %operation, and data %data"
+    export function complete<T>(parray: [Asynchronous.ThenHandler<any, T>, Asynchronous.CatchHandler<any>], operation: Asynchronous.CompleteOperation, data: T) {
+        parray[operation](data);
     };
 };
